e1ffbe5361c112b513fec5434663f4fe5812cd2b
Obakeng007 <obakeng.dusani@outlook.com>
2024-04-02 12:07:53 +0200

    Fixing the struture of the folder and setting up routes using express.

--- app.js
+++ app.js
@@ -3,15 +3,38 @@ const express = require("express");
 //Create express app
 const app = express();
 
+//configure a view
+app.set("view engine", "ejs");
+
 //Create a listerner port
 app.listen(3000);
 
 //Create routes
 app.get("/", (req, res) => {
-  res.sendFile("index.html", { root: __dirname });
+  res.render("index");
   res.status(200);
 });
 
+app.get("/about", (req, res) => {
+  res.status(200).render("about");
+});
+
+app.get("/contact", (req, res) => {
+  res.status(200).render("contact");
+});
+
+app.get("/register", (req, res) => {
+  res.status(200).render("register");
+});
+
+app.get("/login", (req, res) => {
+  res.status(200).render("login");
+});
+
 app.get("/product", (req, res) => {
-  res.sendFile("product.html", { root: __dirname });
+  res.status(200).render("product");
+});
+
+app.get((req, res) => {
+  res.status(404).render("404");
 });
